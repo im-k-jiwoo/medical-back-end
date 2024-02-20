afba822dcbadc600bb9b23525953913863756611
Jiwoo Kim <128121468+im-k-jiwoo@users.noreply.github.com>
2024-02-20 14:53:25 +0900

    Update trivy-image-scan.sh

--- trivy-image-scan.sh
+++ trivy-image-scan.sh
@@ -4,7 +4,15 @@ dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
 ## Trivy 이미지 스캔 실행
-docker run --rm -v $WORKSPACE:/root/.cache/ -e TRIVY_GITHUB_TOKEN="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" aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+# -v $WORKSPACE:/root/.cache/: Trivy 캐시를 Jenkins 워크스페이스로 캐시 공유
+# -q: Trivy의 출력 간소화
+# --exit-code 1: 취약점이 발견되면 'exit_code' 1 반환
+# --severity CRITICAL: 심각도가 CRITICAL인 취약점만 보고
+# --light: 가볍게 스캔, 패키지 버전만 확인
+docker run --rm -v $WORKSPACE:/root/.cache/ -e 
+TRIVY_GITHUB_TOKEN="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" 
+aquasec/trivy:0.17.2 -q image --exit-code 1 
+--severity CRITICAL --light $dockerImageName
 
 ## Trivy 스캔 결과 처리
 exit_code=$?
@@ -17,3 +25,7 @@ if [[ "$exit_code" == 1 ]]; then
 else
     echo "이미지 검사가 통과되었습니다. 심각한 취약점이 발견되지 않았습니다."
 fi
+
+
+
+
